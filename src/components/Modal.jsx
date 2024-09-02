5fdb47acf7574a9e546058a928687159dee2b60c
Kevin Cripe <kevincripe6@gmail.com>
2024-09-02 11:25:19 -0500

    modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,17 +3,18 @@ import { createPortal } from 'react-dom';
 
 const Modal = forwardRef(function Modal({children, buttonCaption }, ref) {
     const dialog = useRef();
+    
     useImperativeHandle(ref, () => {
         return {
             open() {
                 dialog.current.showModal();
             }
-        }
+        };
     });
 
 
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog ref={dialog} className='backdrop:bg-stone-900/90 p-4 rounded-md shadow-md'>
             {children}
             <form method='dialog'>
                 <button>{buttonCaption}</button>
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import {useRef} from 'react';
 import Input from "./Input.jsx"
 import Modal from "./Modal.jsx"
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd }) {
 
     const modal = useRef();
     const title = useRef();
@@ -14,7 +14,11 @@ export default function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+        if (
+            enteredTitle.trim() === '' || 
+            enteredDescription.trim() === '' || 
+            enteredDueDate.trim() === ''
+        ) {
             modal.current.open();
             return;
         }
@@ -28,10 +32,11 @@ export default function NewProject({onAdd}) {
 
     return (
         <>
-        <Modal ref={modal} buttonCaption="Okay"/>
-        <h2>Invalid Input</h2>
-        <p>Oops ... looks like you forgot to enter a value.</p>
-        <p>Please make sure you provide a valid value for every input field.</p>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className='text-xl font-bold text-stone-500 my-4'>Invalid Input</h2>
+            <p className='text-stone-400 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+            <p className='text-stone-400 mb-4' >Please make sure you provide a valid value for every input field.</p>
+        </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
