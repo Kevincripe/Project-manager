c487cbfdfae471eb01219982a34f3238143d4e92
Kevin Cripe <kevincripe6@gmail.com>
2024-09-02 10:32:38 -0500

    error val

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import {useRef} from 'react';
 import Input from "./Input.jsx"
+import Modal from "./Modal.jsx"
 
 export default function NewProject({onAdd}) {
+
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,7 +14,10 @@ export default function NewProject({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        // validation ..
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+            modal.current.open();
+            return;
+        }
 
         onAdd({
             title: enteredTitle,
@@ -21,6 +27,11 @@ export default function NewProject({onAdd}) {
     }
 
     return (
+        <>
+        <Modal ref={modal} buttonCaption="Okay"/>
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
@@ -35,5 +46,6 @@ export default function NewProject({onAdd}) {
                 <Input type="date" ref={dueDate} label="Due Date"/>
             </div>
         </div>
+        </>
     )
 }
